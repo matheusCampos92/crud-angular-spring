75681489b1bbd6ed8c6fcb4b8d4fdd8a8c8aebc8
Matheus Campos <mr.campos92@yahoo.com.br>
2023-01-17 17:11:26 -0300

    first commit

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,35 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+
+import { Course } from './../model/course';
+import { CoursesService } from './../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent {
+
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar os Cursos.')
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+}
